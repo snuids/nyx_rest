b287f9cb73207c85c1d63b8c1ca0476fdb7923da
Arnaud Marchand <amarchand@icloud.com>
2019-08-28 16:54:01 +0200

    Save point

---
+++ sources/lib/ext_test.py
@@ -0,0 +1,20 @@
+import logging
+from flask_restplus import Namespace,Api, Resource, fields
+
+
+print("***>"*100)
+
+logger=logging.getLogger()
+
+def config(api,conn,es,redis,token_required):
+    #---------------------------------------------------------------------------
+    # API configRest
+    #---------------------------------------------------------------------------
+    @api.route('/api/v1/ext/version')
+    @api.doc(description="External version.",params={'token': 'A valid token'})
+
+    class extVersion(Resource):    
+        @token_required()
+        def get(self,user=None):
+            logger.info("Config called")
+            return {'error':"",'status':'ok','version':"1.0",'welcome':"TOTOTTOTO"}
